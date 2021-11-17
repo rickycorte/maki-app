bcf964b52b4cfd3b351bda687676a0dfc4491185
Riccardo <rickycortef@gmail.com>
2021-11-17 11:43:11 +0100

    Moved top details block into a separate widget

---
+++ lib/details_page/cover_info.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:maki/models/anime_details.dart';
+
+//TODO: ho mosso tutto il blocco superiore fino alla descrizione cosi possiamo alterare facilmente tutto il layout di questi elementi
+
+class CoverInfo extends StatelessWidget {
+  AnimeDetails anime;
+
+  CoverInfo({Key? key, required this.anime}) : super(key: key);
+
+  Widget _roundedCover(String url, {double radius = 10}) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(radius),
+      child: Image.network(url, fit: BoxFit.fill),
+    );
+  }
+
+  Widget _scoreAndSeasonInfo(context,
+      {Alignment containerAlignment = Alignment.topRight,
+      TextAlign textAlignment = TextAlign.right}) {
+    String scoreText = "SCORE\n${anime.score}";
+    String seasonText =
+        "SEASON\n${anime.season} ${anime.year ?? "Unknown year"}";
+    String formatText = "FORMAT\n${anime.format ?? "Unknown format"}";
+
+    if (anime.episodeCount != null && anime.episodeMinutes != null) {
+      formatText += "\n${anime.episodeCount} eps of ${anime.episodeMinutes}min";
+    }
+
+    String statusText = "STATUS\n${anime.airStatus}";
+
+    final theme = Theme.of(context);
+
+    return Container(
+        alignment: containerAlignment,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                scoreText,
+                textAlign: textAlignment,
+                style: theme.textTheme.headline6,
+              ),
+            ),
+            Text(
+              seasonText,
+              textAlign: textAlignment,
+              style: theme.textTheme.bodyText1,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              formatText,
+              textAlign: textAlignment,
+              style: theme.textTheme.bodyText1,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              statusText,
+              textAlign: textAlignment,
+              style: theme.textTheme.bodyText1,
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildTitleBlock(context) {
+    // prepare data to output
+    String formattedGenres = "";
+    for (int i = 0; i < anime.genres.length; i++) {
+      formattedGenres += " - " + anime.genres[i];
+    }
+
+    formattedGenres = formattedGenres
+        .substring(3); // remove the " - " characters added by the first element
+
+    return Container(
+        alignment: Alignment.center,
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Text(
+            anime.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          if (formattedGenres.isNotEmpty)
+            Text(
+              formattedGenres,
+              textAlign: TextAlign.center,
+            ),
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    const elementPadding = 20.0;
+
+    return Column(children: [
+      IntrinsicHeight(
+          child: Row(
+        children: [
+          // image
+          Expanded(child: _roundedCover(anime.coverUrl)),
+          // side infos
+          Expanded(child: _scoreAndSeasonInfo(context))
+        ],
+      )),
+      const SizedBox(height: elementPadding),
+      _buildTitleBlock(context),
+
+      const SizedBox(height: elementPadding),
+      Text(
+        anime.description,
+        textAlign: TextAlign.center,
+      )
+
+    ]);
+  }
+}
--- lib/details_page/details_page.dart
+++ lib/details_page/details_page.dart
@@ -3,6 +3,8 @@ import 'package:maki/common/custom_appbar.dart';
 import 'package:maki/details_page/youtube_embedded.dart';
 import 'package:maki/models/anime_details.dart';
 
+import 'cover_info.dart';
+
 // use a steteful page because we may load anime data later than the actual page so a refresh may be needed
 class AnimeDetailsPage extends StatefulWidget {
   AnimeDetails? animeData;
@@ -20,115 +22,25 @@ class AnimeDetailsPage extends StatefulWidget {
 
 // state item
 class _AnimeDetailsPageState extends State<AnimeDetailsPage> {
-  // build the cover hear line with anime cover + side data
-  Widget _buildCoverWithSideInfo() {
-    String scoreText = "SCORE\n${widget.animeData!.score}";
-    String seasonText = "SEASON\n${widget.animeData!.season} ${widget.animeData!.year ?? "Unknown year"}" ;
-    String formatText = "FORMAT\n${widget.animeData!.format ?? "Unknown format"}";
-
-    if (widget.animeData!.episodeCount != null && widget.animeData!.episodeMinutes != null) {
-      formatText += " - ${widget.animeData!.episodeCount} eps of ${widget.animeData!.episodeMinutes}min";
-    }
-
-    String statusText = "STATUS\n${widget.animeData!.airStatus}";
-
-    return IntrinsicHeight(
-        child: Row(
-      children: [
-        // image
-        Expanded(
-            child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Image.network(widget.animeData!.coverUrl, fit: BoxFit.fill),
-        )),
-        // side infos
-        Expanded(
-            child: Container(
-                alignment: Alignment.topRight,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 30),
-                      child: Text(
-                        scoreText,
-                        textAlign: TextAlign.right,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                    ),
-                    Text(seasonText, textAlign: TextAlign.right),
-                    const SizedBox(height: 10),
-                    Text(formatText, textAlign: TextAlign.right),
-                    const SizedBox(height: 10),
-                    Text(statusText, textAlign: TextAlign.right),
-                  ],
-                )))
-      ],
-    ));
-  }
-
-  // build block with title and genres under
-  Widget _buildTitleBlock() {
-    // prepare data to output
-    String formattedGenres = "";
-    for (int i = 0; i < widget.animeData!.genres.length; i++) {
-      formattedGenres += " - " + widget.animeData!.genres[i];
-    }
-
-    formattedGenres = formattedGenres
-        .substring(3); // remove the " - " characters added by the first element
-
-    return Container(
-        alignment: Alignment.center,
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Text(
-            widget.animeData!.title,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          if (formattedGenres.isNotEmpty)
-            Text(
-              formattedGenres,
-              textAlign: TextAlign.center,
-            ),
-        ]));
-  }
-
-  Widget _buildCollapsableDescription() {
-    return Text(
-      widget.animeData!.description,
-      textAlign: TextAlign.center,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     const elementPadding = 20.0;
 
-    return Scaffold(
-        appBar: const CustomAppBar(
-          showBackButton: true,
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(10.0),
-          children: [
-            // image + side informations
-            _buildCoverWithSideInfo(),
-            const SizedBox(height: elementPadding),
+    List<Widget> pageElements = [CoverInfo(anime: widget.animeData as AnimeDetails)]; // top info are guaranteed to be present
 
-            //title and genres
-            _buildTitleBlock(),
+    if(widget.animeData?.trailerUrl != null) {
+      pageElements.add(const SizedBox(height: elementPadding));
+      pageElements.add(YoutubeEmbedded(url: widget.animeData?.trailerUrl ?? ""));
+    }
 
-            const SizedBox(height: elementPadding),
-
-            _buildCollapsableDescription(),
-
-            //TODO: build list in a functon to add items
-            const SizedBox(height: elementPadding),
-
-            YoutubeEmbedded(url: widget.animeData?.trailerUrl ?? "")
-          ],
-        ));
+      return Scaffold(
+          appBar: const CustomAppBar(
+            showBackButton: true,
+          ),
+          body: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: pageElements,
+          ));
   }
 }
