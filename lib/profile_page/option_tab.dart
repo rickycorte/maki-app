d0487f45459b01d26452fd9611c8ad6df628d0c1
Riccardo <rickycortef@gmail.com>
2021-12-05 13:24:46 +0100

    Redesigned options tab

--- lib/profile_page/option_tab.dart
+++ lib/profile_page/option_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maki/details_page/elevated_rounded.dart';
 import 'package:maki/login_page/login_page.dart';
 import 'package:maki/models/user.dart';
 
@@ -37,59 +38,77 @@ class _OptionsTabPageState extends State<OptionsTabPage> {
     );
   }
 
-  Widget _profileAndSettings(BuildContext context) {
-    return Column(
-      children: [
-        imageProfile(widget.profilePicture),
-        // ignore: prefer_const_constructors
-        Center(
-          child: Text(
-            widget.nome,
-            style:
+  Widget _profileBaseInfo(context) {
+    return ElevatedRounded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Column(
+            children: [
+              imageProfile(widget.profilePicture),
+              Center(
+                child: Text(
+                widget.nome,
+                style:
                 const TextStyle(color: Colors.black, height: 2, fontSize: 35),
-          ),
+                ),
+              ),
+              Text("Logged in with Anilist", style: Theme.of(context).textTheme.subtitle2),
+              const SizedBox(height:20),
+              SizedBox(
+                height: 60.0,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  onPressed: () {
+                    User.logout();
+                    LoginPage.refreshLogin(context);
+                  },
+                  child: const Text("Logout"),
+                ),
+              ),
+          ]
         ),
-        Center(
-          child: SwitchListTile(
-            title: const Text(
-              "Force Dark Mode",
-              style: TextStyle(color: Colors.black),
-            ),
-            value: false,
-            onChanged: (bool value) {
-              setState(
-                () {},
-              );
-            },
-          ),
-        ),
-      ],
+      )
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(children: [
-      Padding(
-        padding: const EdgeInsets.only(
-            left: 20.0, right: 20.0, top: 80.0, bottom: 10.0),
-        child: _profileAndSettings(context),
-      ),
-      Positioned(
-        bottom: 20,
-        right: 20,
-        left: 20,
-        child: SizedBox(
-          height: 60.0,
-          child: ElevatedButton(
-            onPressed: () {
-              User.logout();
-              LoginPage.refreshLogin(context);
-            },
-            child: const Text("Logout"),
-          ),
+  Widget _settings(context) {
+    return ElevatedRounded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Settings", style: Theme.of(context).textTheme.headline5),
+            SwitchListTile(
+                title: Text(
+                  "Always use Dark Theme",
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                value: false,
+                onChanged: (bool value) {
+                  setState(
+                        () {},
+                  );
+                },
+              ),
+          ],
         ),
       ),
-    ]);
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: const EdgeInsets.only(right: 10, left: 10, top: kToolbarHeight + 15 ),
+            child: Column (
+              children: [
+                _profileBaseInfo(context),
+                const SizedBox(height: 20),
+                _settings(context),
+              ]
+            ),
+        );
   }
 }
