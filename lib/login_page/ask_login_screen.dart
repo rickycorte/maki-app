71954fdf94293d0eea7d39b2a199d3737b60844c
Riccardo <rickycortef@gmail.com>
2021-12-13 17:40:35 +0100

    Fixed login page layout

--- lib/login_page/ask_login_screen.dart
+++ lib/login_page/ask_login_screen.dart
@@ -14,43 +14,43 @@ class AskLoginScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Stack(
-        children: [ Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
+        children: [ ListView(
+          primary: false,
           children: [
-            Padding(
-                padding: const EdgeInsets.only(top: 80.0, left: 100.0, right: 100.0, bottom: 60.0),
-                child: Image.asset("assets/images/login_hi.png")
+            Container(
+              constraints: const BoxConstraints(maxHeight: 450),
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 80.0, left: 100.0, right: 100.0, bottom: 60.0),
+                  child: Image.asset("assets/images/login_hi.png")
+              ),
             ),
             Center(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text("Hi sen(pi)~", style: Theme.of(context).textTheme.headline4),
-                  const SizedBox(height: 10.0),
-                  Text("Please login with your anilist account to:", style: Theme.of(context).textTheme.subtitle1),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 3/4,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.star, color: Theme.of(context).primaryColor),
-                          title: const Text("Get personalize recommendations"),
-                          minLeadingWidth: 5,
+                  const SizedBox(height: 15.0),
+                  Text("Please login with your Anilist account to:", style: Theme.of(context).textTheme.subtitle1),
+                  const SizedBox(height: 10,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      "Get personalized recommendations",
+                      "Access your anime list within the app",
+                      "Add and remove anime to your list"
+                    ].map((msg) =>
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.star, color: Theme.of(context).primaryColor),
+                              const SizedBox(width: 5,),
+                              Text(msg, style: Theme.of(context).textTheme.subtitle1)
+                            ],
+                          ),
                         ),
-                        ListTile(
-                          leading: Icon(Icons.star, color: Theme.of(context).primaryColor),
-                          title: const Text("Get fast access to your list"),
-                          minLeadingWidth: 5,
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.star, color: Theme.of(context).primaryColor),
-                          title: const Text("Add suggested anime to your list"),
-                          minLeadingWidth: 5,
-                        ),
-                      ],
-                    ),
+                    ).toList(),
                   ),
                 ],
               ),
