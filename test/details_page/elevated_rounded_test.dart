a502241913de7e3225a41996dbca857632dba9d4
Riccardo <rickycortef@gmail.com>
2021-12-18 18:28:04 +0100

    Added elevetaded rounded tests

---
+++ test/details_page/elevated_rounded_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:maki/details_page/elevated_rounded.dart';
+
+import 'package:maki/main.dart';
+
+import '../utils.dart';
+
+void main() {
+  testWidgets('Counter contain child', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(createTestWrapper(const ElevatedRounded(child: Text("Racconzzz"))));
+
+    // Verify that our counter starts at 0.
+    expect(find.text("Racconzzz"), findsOneWidget);
+
+  });
+}
